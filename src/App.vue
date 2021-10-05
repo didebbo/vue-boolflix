c1d90a5395c0e4508da34e834b7699b5303610d2
didebbo <gianluca.napoletano.93@gmail.com>
2021-10-05 16:17:25 +0200

    refactoring axios

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,13 @@
   <div id="app">
     <Header @getQuery="getQuery" />
     <main>
-      <Movies :movies="movies" />
-      <Tvs :tvs="tvs" />
+      <Movies :query="query" />
+      <Tvs :query="query" />
     </main>
   </div>
 </template>
 
 <script>
-import axios from "axios";
 import Header from "./components/Header.vue";
 import Movies from "./components/Movies.vue";
 import Tvs from "./components/Tvs.vue";
@@ -24,43 +23,11 @@ export default {
   data() {
     return {
       query: "",
-      movies: [],
-      tvs: [],
     };
   },
   methods: {
     getQuery(query) {
       this.query = query;
-      this.getMovies();
-      this.getTvs();
-    },
-    getMovies() {
-      axios
-        .get("https://api.themoviedb.org/3/search/movie", {
-          params: {
-            api_key: "af0ba66c25483bbc937edba39186698d",
-            language: "it-IT",
-            query: this.query,
-          },
-        })
-        .then((res) => {
-          console.log(res.data.results);
-          this.movies = res.data.results;
-        });
-    },
-    getTvs() {
-      axios
-        .get("https://api.themoviedb.org/3/search/tv", {
-          params: {
-            api_key: "af0ba66c25483bbc937edba39186698d",
-            language: "it-IT",
-            query: this.query,
-          },
-        })
-        .then((res) => {
-          // console.log(res.data.results);
-          this.tvs = res.data.results;
-        });
     },
   },
 };
--- src/components/Tvs.vue
+++ src/components/Tvs.vue
@@ -6,12 +6,34 @@
 </template>
 
 <script>
+import axios from "axios";
 import Tv from "./Tv.vue";
 export default {
   name: "Tvs",
-  props: ["tvs"],
+  props: ["query"],
   components: {
     Tv,
+  },
+  data() {
+    return {
+      tvs: [],
+    };
+  },
+  watch: {
+    query() {
+      axios
+        .get("https://api.themoviedb.org/3/search/tv", {
+          params: {
+            api_key: "af0ba66c25483bbc937edba39186698d",
+            language: "it-IT",
+            query: this.query,
+          },
+        })
+        .then((res) => {
+          // console.log(res.data.results);
+          this.tvs = res.data.results;
+        });
+    },
   },
 };
 </script>

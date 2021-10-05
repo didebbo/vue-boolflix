b8da2436693d7ca8e1926c3ea29cc08c9e7910fa
didebbo <gianluca.napoletano.93@gmail.com>
2021-10-05 14:34:38 +0200

    completata milestone 1

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="movie">
-    <h4>This is the movie component</h4>
-  </div>
+  <ul class="movie">
+    <li>
+      {{ `ID: ${movie.id}` }}
+    </li>
+    <li>
+      {{ `Title: ${movie.title}` }}
+    </li>
+    <li>
+      {{ `Original Title: ${movie.original_title}` }}
+    </li>
+    <li>
+      {{ `Language: ${movie.original_language}` }}
+    </li>
+    <li>
+      {{ `Vote Average: ${movie.vote_average}` }}
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: "Movie",
+  props: ["movie"],
 };
 </script>
 
--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="movies">
-    <h3>This is the movies section</h3>
-    <Movie />
+    <Movie v-for="(movie, index) in movies" :key="index" :movie="movie" />
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import Movie from "./Movie.vue";
 export default {
   name: "Movies",
@@ -13,9 +13,26 @@ export default {
   components: {
     Movie,
   },
+  data() {
+    return {
+      movies: [],
+    };
+  },
   watch: {
     query() {
       console.log(this.query);
+      axios
+        .get("https://api.themoviedb.org/3/search/movie", {
+          params: {
+            api_key: "af0ba66c25483bbc937edba39186698d",
+            language: "it-IT",
+            query: this.query,
+          },
+        })
+        .then((res) => {
+          console.log(res.data.results);
+          this.movies = res.data.results;
+        });
     },
   },
 };
